47bbbafaaa3e07fd19779f4eb2111480aa726df4
Prosper <pblinkprosper01@gmail.com>
2023-11-26 22:40:12 +0000

    config client ssh without password

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# Configure client SSH to connect to server without typing a password.
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  }
