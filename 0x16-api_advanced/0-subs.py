d4b859a630a24a50b7fe151d051fecdb7889c0b3
pblinkprosper <pblinkprosper01@gmail.com>
2024-11-30 10:53:22 +0000

    updated files

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and returns the
-number of subscribers for a given subreddit"""
-import requests
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API to get the number of subscribers
-    for a given subreddit.
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
-    Args:
-        subreddit (str): The name of the subreddit
-        (e.g., 'python', 'programming').
-
-    Returns:
-        int: Number of subscribers for the subreddit.
-    """
-    try:
-        url = f"https://www.reddit.com/r/{subreddit}/about.json"
-        response = requests.get(url, headers={"User-Agent": "MyRedditBot"})
-        data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
-    except Exception as e:
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
+
+    return sub_info.json().get("data").get("subscribers")
